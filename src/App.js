4507584d25f4935816cee3bed5f87ff7703a2a67
Dan Wood <daniel4wood@icloud.com>
2021-05-14 22:12:35 +1000

    Friday work

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [name, setName] = useState("default");
+  const [pokemon, setPokemon] = useState(null);
+  console.log(pokemon);
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+      .then((result) => result.json())
+      .then((json) => setPokemon(json));
+  }, []);
+
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center" }}>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          fetch("https://pokeapi.co/api/v2/pokemon/" + name)
+            .then((result) => result.json())
+            .then((json) => setPokemon(json));
+        }}
+      >
+        Submit
+      </button>
+      <div>{pokemon?.name}</div>
     </div>
   );
 }
